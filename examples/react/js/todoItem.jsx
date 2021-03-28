4ee910f1ff50a4827804e4bdc258094fd2110988
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2021-03-28 18:52:11 -0400

    Ordering the functions in your class

--- examples/react/js/todoItem.jsx
+++ examples/react/js/todoItem.jsx
@@ -12,67 +12,8 @@ var app = app || {};
 	var ENTER_KEY = 13;
 
 	app.TodoItem = React.createClass({
-		handleSubmit: function (event) {
-			var val = this.state.editText.trim();
-			if (val) {
-				this.props.onSave(val);
-				this.setState({editText: val});
-			} else {
-				this.props.onDestroy();
-			}
-		},
-
-		handleEdit: function () {
-			this.props.onEdit();
-			this.setState({editText: this.props.todo.title});
-		},
-
-		handleKeyDown: function (event) {
-			if (event.which === ESCAPE_KEY) {
-				this.setState({editText: this.props.todo.title});
-				this.props.onCancel(event);
-			} else if (event.which === ENTER_KEY) {
-				this.handleSubmit(event);
-			}
-		},
-
-		handleChange: function (event) {
-			if (this.props.editing) {
-				this.setState({editText: event.target.value});
-			}
-		},
-
 		getInitialState: function () {
 			return {editText: this.props.todo.title};
-		},
-
-		/**
-		 * This is a completely optional performance enhancement that you can
-		 * implement on any React component. If you were to delete this method
-		 * the app would still work correctly (and still be very performant!), we
-		 * just use it as an example of how little code it takes to get an order
-		 * of magnitude performance improvement.
-		 */
-		shouldComponentUpdate: function (nextProps, nextState) {
-			return (
-				nextProps.todo !== this.props.todo ||
-				nextProps.editing !== this.props.editing ||
-				nextState.editText !== this.state.editText
-			);
-		},
-
-		/**
-		 * Safely manipulate the DOM after updating the state when invoking
-		 * `this.props.onEdit()` in the `handleEdit` method above.
-		 * For more info refer to notes at https://facebook.github.io/react/docs/component-api.html#setstate
-		 * and https://facebook.github.io/react/docs/component-specs.html#updating-componentdidupdate
-		 */
-		componentDidUpdate: function (prevProps) {
-			if (!prevProps.editing && this.props.editing) {
-				var node = React.findDOMNode(this.refs.editField);
-				node.focus();
-				node.setSelectionRange(node.value.length, node.value.length);
-			}
 		},
 
 		render: function () {
@@ -104,5 +45,64 @@ var app = app || {};
 				</li>
 			);
 		}
+
+		handleEdit: function () {
+			this.props.onEdit();
+			this.setState({editText: this.props.todo.title});
+		},
+
+		handleChange: function (event) {
+			if (this.props.editing) {
+				this.setState({editText: event.target.value});
+			}
+		},
+
+		handleKeyDown: function (event) {
+			if (event.which === ESCAPE_KEY) {
+				this.setState({editText: this.props.todo.title});
+				this.props.onCancel(event);
+			} else if (event.which === ENTER_KEY) {
+				this.handleSubmit(event);
+			}
+		},
+
+		handleSubmit: function (event) {
+			var val = this.state.editText.trim();
+			if (val) {
+				this.props.onSave(val);
+				this.setState({editText: val});
+			} else {
+				this.props.onDestroy();
+			}
+		},
+
+		/**
+		 * This is a completely optional performance enhancement that you can
+		 * implement on any React component. If you were to delete this method
+		 * the app would still work correctly (and still be very performant!), we
+		 * just use it as an example of how little code it takes to get an order
+		 * of magnitude performance improvement.
+		 */
+		shouldComponentUpdate: function (nextProps, nextState) {
+			return (
+				nextProps.todo !== this.props.todo ||
+				nextProps.editing !== this.props.editing ||
+				nextState.editText !== this.state.editText
+			);
+		},
+
+		/**
+		 * Safely manipulate the DOM after updating the state when invoking
+		 * `this.props.onEdit()` in the `handleEdit` method above.
+		 * For more info refer to notes at https://facebook.github.io/react/docs/component-api.html#setstate
+		 * and https://facebook.github.io/react/docs/component-specs.html#updating-componentdidupdate
+		 */
+		componentDidUpdate: function (prevProps) {
+			if (!prevProps.editing && this.props.editing) {
+				var node = React.findDOMNode(this.refs.editField);
+				node.focus();
+				node.setSelectionRange(node.value.length, node.value.length);
+			}
+		},
 	});
 })();
